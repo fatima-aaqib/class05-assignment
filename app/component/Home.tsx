4304729dcaf07e7ef64042175a606ed6ecb0cf22
fatima tuz zohra <hashiraaqib123@gmail.com>
2024-10-31 21:08:53 +0500

    first commit

---
+++ app/component/Home.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+export default function Home() {
+    return (
+      
+        
+              
+              
+              <header>
+                
+          
+              
+                
+                
+              
+          
+          
+              <div>
+                
+          
+          <nav className="flex w-full bg-sky-800 h-20 items-center rounded-md">
+            <div className="bg-red-400 h-9 w-44 rounded-xl">
+              <p className="flex justify-center text-black font-bold">
+                HOME
+              </p>
+            </div>
+          
+          
+          
+          
+            <div className="flex justify-end w-4/5 items-center">
+            
+          
+            
+            
+            <div className="flex justify-start  bg-yellow-500 h-9 w-44 rounded-xl">
+              <p className="flex justify-between text-black font-bold"> services </p></div>
+              
+              
+            
+              <div className="flex justify-center bg-green-500 h-9 w-44 rounded-xl">
+          <p className="flex justify-between text-black font-bold">contact-us</p>
+          </div>
+          
+          
+            
+          <div className="flex justify-end bg-blue-600 h-9 w-44 rounded-xl">
+            <p className="flex justify-between text-black font-bold">About me</p>
+            
+          </div>
+          
+          </div>
+          </nav>
+          <br />
+          <br />
+          
+          <br />
+          <div>
+          
+          <ul className="flex space-x-10 px-4 w-full justify-center">
+          
+            <li className="bg-cyan-300 w-1/3 h-48"></li>
+          
+            <li className="bg-lime-400 w-1/3 h-48"></li>
+          
+          
+          
+          
+          </ul>
+          
+          
+          <br />
+          <br />
+          <ul className="flex space-x-11 px-4 w-full justify-between">
+          
+          
+            <li className="bg-fuchsia-900 w-1/2 h-64">
+          
+            </li>
+          
+            <li className="bg-teal-500 w-1/2 h-64"></li>
+            <li className="bg-pink-700 w-1/2 h-64"></li>
+          
+          
+          
+          </ul>
+          
+          
+          </div>
+          
+          
+           </div>
+          
+          
+          
+           
+           
+          
+           </header>
+           
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+           
+          
+          
+          
+              
+      
+  
+  
+  
+    )
+  }
+      
---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import Home from "./component/Home";
+
+export default function car() {
+  return (
+    
+    
+   <div>
+              
+              
+<Home />
+
+
+   </div>
+ 
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+  )
+}
+    
